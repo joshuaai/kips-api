7430f719ed316441eca5bf70d5edc3c0166c8d08
joshuaai <joshuaiheji@gmail.com>
2017-05-10 07:36:35 +0000

    created lib folder in app for jwt

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,6 @@ module KipsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.autoload_paths << Rails.root.join('lib')
     
     config.middleware.insert_before 0, Rack::Cors do
       allow do
