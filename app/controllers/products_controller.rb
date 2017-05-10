d1b6f2adfe3ed00b2ba753436c0376e48a209792
joshuaai <joshuaiheji@gmail.com>
2017-05-10 13:09:34 +0000

    products email

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -47,6 +47,6 @@ class ProductsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def product_params
-      params.require(:product).permit(:name)
+      params.require(:product).permit(:name, :email)
     end
 end
